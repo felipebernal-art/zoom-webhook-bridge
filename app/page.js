eb0f3942a3278ad6fb58054b5debe90d854abac8
felipebernal-art <felipebernal@investepacademy.com>
2026-01-31 10:23:01 -0500

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ fontFamily: "sans-serif", padding: 24 }}>
+      <h1>Zoom Webhook Bridge</h1>
+      <p>Status: OK</p>
+    </main>
+  );
+}
